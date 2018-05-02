0759321a7ab684307bc54aa8083c349194f60001
Juno Lee <lejuno80@gmail.com>
2018-05-01 19:48:39 -0500

    modified events

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,9 +1,11 @@
 const loadFishes = require('./fishes');
 const writeFishes = require('./fishDom');
+const bindEvents = require('./events');
 
 const whenFishesLoad = (data) => {
   console.log('data', data);
   $('#available').append(writeFishes(data.fishes));
+  bindEvents();
 
 };
 
@@ -13,6 +15,7 @@ const whenFishesDontLoad = (error) => {
 
 const initializer = () => {
   loadFishes(whenFishesLoad, whenFishesDontLoad);
+
 };
 
 module.exports = initializer;
